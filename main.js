1b2ccdf68eda989d4e8f99b9131b4d17aee4f387
Gabriel Osorio <gabriel.osorio@neonroots.com>
2013-06-03 15:49:20 -0300

    Implemented webcam to canvas functionality.

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
-  var videoElement = document.getElementById('webcam');
+var canvasVideo = function() {
+  console.log("starting");
+
+  var $ = jQuery,
+  video = document.getElementById('canvasVideo'),
+  canvas = document.getElementById('canvasVideoCvs'),
+  backcvs = document.getElementById('canvasVideoBcvs'),
+  ctx = canvas.getContext('2d'),
+  bcv = backcvs.getContext('2d'),
+  w = canvas.width,
+  h = canvas.height;
 
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
   navigator.getUserMedia({
     video: true,
   }, function(localMediaStream) {
-    // Success Callback
-    videoElement.src = window.URL.createObjectURL(localMediaStream);
-
-    console.log("Bang!");
-
-    videoElement.onloadmetadata = function() {
-      console.log("Frame!");
-    }
+    video.src = window.URL.createObjectURL(localMediaStream);
+    drawFrame();
   }, function(e) {
-    // Error Callback
     if (e.code = 1) {
       console.log("User declined permission.");
     }
   });
+
+  function reloadMode() {
+    video.pause();
+    var wait = setTimeout(function(){
+    video.play();
+    }, 5);
+  }
+
+  function drawFrame() {
+    bcv.drawImage(video, 0, 0, w, h);
+    var apx = bcv.getImageData(0, 0, w, h);
+    ctx.putImageData(apx, 0, 0);
+    setTimeout(function() {
+      drawFrame();
+    }, 60);
+  }
+
+  function drawFrameWithBorders() {
+    bcv.drawImage(video, 0, 0, w, h);
+    var apx = bcv.getImageData(0, 0, w, h);
+    ctx.putImageData(apx, 0, 0);
+    setTimeout(function() {
+      drawFrame();
+    }, 60);
+  }
+}
+
+jQuery(function() {
+  canvasVideo();
+});
